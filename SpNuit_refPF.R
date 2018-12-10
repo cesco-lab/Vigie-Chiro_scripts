52b5440c3c32ecf3120157117246dede9d787080
YvesBas <yves.bas@gmail.com>
2018-12-10 13:52:07 +0100

    add average activity and standard deviation

--- SpNuit_refPF.R
+++ SpNuit_refPF.R
@@ -2,7 +2,7 @@ library(data.table)
 ### A FAIRE :
 SeuilFiable="Seuil90"
 
-SpNuit=fread("DataPF_SpNuit2.csv")
+SpNuit=fread("SpNuit2_Seuil90_DataLP_PF_exportTot.csv")
 
 hist(SpNuit$decalage_debut_coucher,xlim=c(-5000,10000),breaks=200)
 hist(SpNuit$decalage_fin_lever,xlim=c(-5000,10000),breaks=200)
@@ -15,6 +15,8 @@ SpNuit[,DecMin:=DecMin]
 test=subset(SpNuit,SpNuit$espece=="Rhifer")
 plot(test$DecMin,test$nb_contacts,xlim=c(-6000,6000),log="y")
 
+NbNuit=nlevels(as.factor(paste(SpNuit$participation,SpNuit$Nuit,SpNuit$num_micro)))
+
 i=1
 Q25=vector()
 Q75=vector()
@@ -23,6 +25,10 @@ DM25=vector()
 DM10=vector()
 DM02=vector()
 nbocc=vector()
+MoySiP=vector()
+EtypSiP=vector()
+MoyG=vector()
+EtypG=vector()
 for (i in 1:nlevels(as.factor(SpNuit$espece)))
 {
   Datasub=subset(SpNuit,SpNuit$espece==levels(as.factor(SpNuit$espece))[i])
@@ -32,14 +38,20 @@ for (i in 1:nlevels(as.factor(SpNuit$espece)))
   Q98=c(Q98,quantile(Datasub$nb_contacts,0.98))
   print(paste(i,"/",nlevels(as.factor(SpNuit$espece))))
   nbocc=c(nbocc,nrow(Datasub))
+  MoySiP=c(MoySiP,mean(Datasub$nb_contacts))
+  EtypSiP=c(EtypSiP,sd(Datasub$nb_contacts))
+  MoyG=c(MoyG,mean(c(Datasub$nb_contacts,rep(0,NbNuit-nrow(Datasub)))))
+  EtypG=c(EtypG,sd(c(Datasub$nb_contacts,rep(0,NbNuit-nrow(Datasub)))))
   #filtrage des problèmes d'heures
   Datasub2=subset(Datasub,Datasub$DecMin>(-1800))
   #calcul des quantiles de décalage de temps minimum avec coucher-lever
   DM25=c(DM25,quantile(Datasub2$DecMin,0.25))
   DM10=c(DM10,quantile(Datasub2$DecMin,0.10))
   DM02=c(DM02,quantile(Datasub2$DecMin,0.02))
+  
+  
   }
 
-Ref=cbind(Espece=levels(as.factor(SpNuit$espece)),Q25,Q75,Q98,nbocc
+Ref=cbind(Espece=levels(as.factor(SpNuit$espece)),MoyG,EtypG,MoySiP,EtypSiP,Q25,Q75,Q98,nbocc
           ,DM25,DM10,DM02)  
 write.csv(Ref,paste0("refPF_",SeuilFiable,".csv"))
