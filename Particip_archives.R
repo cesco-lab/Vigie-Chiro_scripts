6563ded9b20a8a3ee4050546be5466329ceef95e
YvesBas <yves.bas@gmail.com>
2018-12-23 10:43:11 +0100

    add validated status

--- Particip_archives.R
+++ Particip_archives.R
@@ -4,6 +4,9 @@ library(data.table)
 Particip=fread("C:/wamp64/www/p_export.csv")
 A_ta=fread("C:/wamp64/www/archiveesta.txt",header=F)
 A_wav=fread("C:/wamp64/www/archiveeswav.txt",header=F)
+Valid=fread("export_validtot2018-09-03.csv")
+
+Particip$Valid=(Particip$participation %in% Valid$participation)
 
 #P_A_ta=subset(Particip,Particip$participation %in% A_ta$V1)
 #P_A_wav=subset(Particip,Particip$participation %in% A_wav$V1)
