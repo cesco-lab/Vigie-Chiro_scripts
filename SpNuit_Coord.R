cfa563541617427e72ac273ec636b47783131f68
YvesBas <yves.bas@gmail.com>
2018-12-26 10:57:13 +0100

    add ALL option

--- SpNuit_Coord.R
+++ SpNuit_Coord.R
@@ -8,17 +8,27 @@ library(data.table)
 #pour afficher les milisecondes
 op <- options(digits.secs=3)
 
-args="SpNuit2_Seuil50_DataLP_PF_exportTot"
-#recupération des données chiros
-DataCPL3=fread(paste0(args,".csv"))
 #récupération des données participation
 Particip=fread("C:/wamp64/www/p_export.csv")
-  #récupération des localités
+#récupération des localités
 SiteLoc=fread("C:/wamp64/www/sites_localites.txt")
+
+#args="SpNuit2_Seuil50_DataLP_PF_exportTot"
+args="ALL"
+
+if(args="ALL")
+{
+  SelPar=Particip  
+}else{
+#recupération des données chiros
+DataCPL3=fread(paste0(args,".csv"))
 
 #liste des coordonnées existantes dans ce jeu de données
 ListPar=levels(as.factor(DataCPL3$participation))
+
 SelPar=subset(Particip,Particip$participation %in% ListPar)
+}
+
 SelParSL=merge(SiteLoc,SelPar,by.x=c("site","nom"),by.y=c("site","point"))
 CoordCPL3=aggregate(SelParSL$participation
                 ,by=c(list(SelParSL$longitude),list(SelParSL$latitude))
