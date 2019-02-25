67d9e15ed8be75d1d6cb5df46776a56237eba74c
YvesBas <yves.bas@gmail.com>
2019-02-25 22:12:45 +0100

    add tagging and merging options

--- Extr_sel.R
+++ Extr_sel.R
@@ -1,19 +1,65 @@
 library(data.table)
+library(readxl)
 
 #ETAPE 0 - IMPORT DES TABLES
 #bien renommer les chemins en fonction de l'ordi utilisé
 #et vérifier les versions (date, import complet ou non)
-args="C:/wamp64/www/Sel_validobs_2017.csv"
-  
+args="./Tadarida/rounds/compli_validation190220/Round190220.csv"
+
 #table "données"
-DataTot=fread("C:/wamp64/www/export180227.txt")
+DataTot=fread("C:/wamp64/www/export.txt")
 Sys.time()
+Tag=F
+LocTag="C:/wamp64/www/SelValid190221.csv"
+Merge=T
+
+
+if(Tag){
+FTag=fread(LocTag)
+FTag$Sel="x"
+FTag=unique(FTag)
+}
+
+
+names(DataTot)[10]="temps_fin"
 
 Sel=fread(args)
+test1=match("Filename",names(Sel))
+test2=match("donnee",names(Sel))
+if(is.na(test1))
+{
+  if(is.na(test2))
+  {
+    names(Sel)[1]="Filename"
+  }else{
+    names(Sel)[test2]="Filename"
+  }
+}
+
+if(grepl(".wav",Sel$Filename[1]))
+{
+  Sel$Filename=substr(Sel$Filename,1,nchar(Sel$Filename)-4)
+}
 
 #tol=0 #nombre de données à garder avant/après
 
 #test=match(Sel$Filename,DataTot$donnee)
 
 DataSel=subset(DataTot,DataTot$donnee %in% Sel$Filename)
-fwrite(DataSel,paste0(dirname(args),"/Data_",basename(args)))
+
+
+if(Tag)
+{
+DataSel2=merge(DataSel,FTag,by="donnee",all.x=T)
+}else{
+  DataSel2=DataSel
+}
+
+
+if(Merge)
+{
+  DataSel2=merge(DataSel2,Sel,by.x=c("donnee","espece","frequence")
+                 ,by.y=c("Filename","SpMaxF2","FreqM"),all.x=T)
+}
+
+fwrite(DataSel2,paste0(dirname(args),"/Data_",basename(args)))
