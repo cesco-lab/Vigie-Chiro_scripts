c7177906e3e6db1f5a654b649c8fbfe783dc1092
YvesBas <yves.bas@gmail.com>
2019-04-15 15:34:51 +0200

    add a plot for checking

--- combineGIS.R
+++ combineGIS.R
@@ -1,12 +1,15 @@
 library(data.table)
+library(sp)
 listGI=c("ALAN","Alti","Bioclim","Carthage","CLC","OCS")
-Points="RandPts_France_dep_L93Radius_ 28000_1000"
-Coord=c("Group.1","Group.2")
-#Coord=c("decimalLongitude", "decimalLatitude")
+#Points="RandPts_France_dep_L93Radius_ 91000_1000"
+Points="PA_Thymus nitens"
+#Coord=c("Group.1","Group.2")
+Coord=c("decimalLongitude", "decimalLatitude")
 
 
 tab1=fread(paste0("./VigieChiro/GIS/",Points,".csv"))
 tab1=unique(tab1,by=Coord)
+
 
 for (i in 1:length(listGI))
 {
@@ -16,3 +19,7 @@ for (i in 1:length(listGI))
 }
 
 fwrite(tab1,paste0("./VigieChiro/GIS/GI_",Points,".csv"))
+coordinates(tab1)=Coord
+SelCol=sample(names(tab1),1)
+spplot(tab1,zcol=SelCol,main=SelCol)
+
