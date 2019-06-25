97835f04f0108c59ed034d33d66623dccca66607
nicolasdubos <37069622+nicolasdubos@users.noreply.github.com>
2019-06-25 16:26:00 +0200

    Create Occ_Thermal_indices_worldclim.R.R

---
+++ Occ_Thermal_indices_worldclim.R.R
@@ -0,0 +1,54 @@
+library(data.table)
+library(mapdata)
+library(reshape)
+library(raster)
+
+# if a file with species names is available:
+species <- levels(factor(data$species))
+# otherwise: 
+species <- c("Eptesicus serotinus", "Eptesicus nilssonii", "Nyctalus lasiopterus", "Nyctalus noctula", "Nyctalus leisleri", 
+             "Pipistrellus kuhlii", "Pipistrellus nathusii", "Pipistrellus pygmaeus", "Plecotus auritus",
+             "Plecotus austriacus", "Rhinolophus ferrumequinum", "Rhinolophus hipposideros", "Tadarida teniotis",
+             "Hypsugo savii", "Myotis alcathoe", "Plecotus macrobullaris", "Plecotus macrobullaris")
+
+
+thermal_indices <- array(dim = c(nlevels(as.factor(species)), 6, 3),
+                         dimnames = list(levels(as.factor(species)),
+                                         c("ThAve", "ThSD", "ThQ95", "ThQ5", "Thrange", "Thmed"),
+                                         c("Yearly", "Summer", "Winter")))
+
+
+for(i in species){
+  print(i)
+  time0 <-  Sys.time()
+  occ <- read.csv(paste0("F:/bat veolia mnhn/gbif occurrences/", i, ".csv"), sep = "\t") #reading occurrence data after download from gbif
+  occ <- occ[complete.cases(occ$decimalLatitude), ]
+  map("worldHires", xlim = c(-20, 59), ylim = c(35, 71)) # visualising the data in Europe
+  points(occ$decimalLatitude ~ occ$decimalLongitude)
+  
+#TO DO: USE MORE PRECISE BIOCLIM LAYER
+Bioclimdir="F:/Climate data/worldclim/wc2.0_10m_bio" #where bioclim TIFF files are
+
+coordinates(occ) <- c("decimalLongitude", "decimalLatitude")
+proj4string(occ) <- CRS("+init=epsg:4326") # WGS 84
+
+ListTifiles=list.files(Bioclimdir,full.names=T)
+
+for(j in c(1,5,6)){  # 1, 5 and 6 for the bioclim variable we use (annual mean T, max T and min T)
+  
+  SpBioci  <- extract(raster(ListTifiles[j]),occ)
+
+  j <- ifelse(j == 1, j, ifelse(j == 5, 2, 3))  # change bioclim numbers for the array
+  thermal_indices[i, 1, j] <- mean(na.omit(SpBioci))
+  thermal_indices[i, 2, j] <- sd(na.omit(SpBioci))
+  thermal_indices[i, 3, j] <- quantile(SpBioci,0.95, na.rm = T)
+  thermal_indices[i, 4, j] <- quantile(SpBioci,0.05, na.rm = T)
+  thermal_indices[i, 5, j] <- thermal_indices[i, 3, j] - thermal_indices[i, 4, j] 
+  thermal_indices[i, 6, j] <- (thermal_indices[i, 3, j] + thermal_indices[i, 4, j])/2
+ }
+save(thermal_indices, file = "F:/bat veolia mnhn/data/thermal indices/thermal_indices.bioclim")
+print(thermal_indices)
+print(Sys.time() - time0)
+}
+
+
