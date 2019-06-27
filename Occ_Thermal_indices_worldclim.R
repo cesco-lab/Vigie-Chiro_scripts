c2def8ccd17c57da8150a2c739985b332bd82cf6
nicolasdubos <37069622+nicolasdubos@users.noreply.github.com>
2019-06-27 16:14:09 +0200

    Update Occ_Thermal_indices_worldclim.R

--- Occ_Thermal_indices_worldclim.R
+++ Occ_Thermal_indices_worldclim.R
@@ -3,13 +3,18 @@ library(mapdata)
 library(reshape)
 library(raster)
 
-species <- c("Eptesicus serotinus", "Eptesicus nilssonii", "Nyctalus lasiopterus", "Nyctalus noctula", "Nyctalus leisleri", 
+# saving a species list
+ # if a 'species' column is available
+species <- levels(as.factors(data$species))
+ # otherwise:
+species <- c("Barbastella", "Eptesicus bottae", "Eptesicus serotinus", "Eptesicus nilssonii", "Nyctalus lasiopterus", "Nyctalus noctula", "Nyctalus leisleri", 
              "Pipistrellus kuhlii", "Pipistrellus nathusii", "Pipistrellus pygmaeus", "Plecotus auritus",
              "Plecotus austriacus", "Rhinolophus ferrumequinum", "Rhinolophus hipposideros", "Tadarida teniotis",
-             "Hypsugo savii", "Myotis alcathoe", "Plecotus macrobullaris", "Rhinolophus euryale",
-             "Rhinolophus melelyi")
+             "Rhinolophus melelyi", "Hypsugo savii", "Myotis alcathoe", "Plecotus macrobullaris", "Rhinolophus euryale",
+             "Miniopterus schreibersii", "Myotis bechsteinii", "Myotis brandtii", "Myotis capaccinii",
+             "Myotis daubentonii", "Myotis emarginatus", "Myotis myotis", "Myotis mystacinus", "Myotis nattereri", "Myotis dasycneme")
 
-
+# creating an array to store the results
 thermal_indices <- array(dim = c(nlevels(as.factor(species)), 6, 3),
                          dimnames = list(levels(as.factor(species)),
                                          c("ThAve", "ThSD", "ThQ95", "ThQ5", "Thrange", "Thmed"),
@@ -21,21 +26,29 @@ for(i in species){
   t0 <-   Sys.time()
   occ <- read.csv(paste0("F:/bat veolia mnhn/gbif occurrences/", i, ".csv"), sep = "\t") #reading occurrence data after download from gbif
   occ <- occ[complete.cases(occ$decimalLatitude), ]
-  map("worldHires", xlim = c(-20, 59), ylim = c(35, 71)) # visualising the data in Europe
-  points(occ$decimalLatitude ~ occ$decimalLongitude)
+  occ <- occ[occ$decimalLatitude > 30 & 
+               occ$decimalLongitude > -20 &
+               occ$decimalLongitude < 70, ]  # Europe and surrounings only
   
-#TO DO: USE MORE PRECISE BIOCLIM LAYER
+    map("worldHires", xlim = c(-20, 59), ylim = c(35, 71)) # visualising the data in Europe
+  points(occ$decimalLatitude ~ occ$decimalLongitude)
+  text(0, 70 ,i)
+  
 Bioclimdir="F:/Climate data/worldclim/wc2.0_10m_bio" #where bioclim TIFF files are
-
-coordinates(occ) <- c("decimalLongitude", "decimalLatitude")
-proj4string(occ) <- CRS("+init=epsg:4326") # WGS 84
 
 ListTifiles=list.files(Bioclimdir,full.names=T)
 
+ #Selecting one value of environmental variable per occurrence pixel
+ #(controls for spatial bias in non-standardised occurrence records (e.g., gbif))
+occ.raster <- rasterize(x = occ[, c("decimalLongitude", "decimalLatitude")],
+                 y = raster(ListTifiles[1]),
+                 fun = function(x, ...) 1) # Bien vÃ©rifier le rÃ©sultat et ajuster la fonction
+occ.unique <- xyFromCell(occ.raster, Which(occ.raster == 1,
+                                           cells = TRUE))
+
 for(j in c(1,5,6)){
-  SpBioci  <- extract(raster(ListTifiles[j]),occ)
-  Sys.time()
-  j <- ifelse(j == 1, j, ifelse(j == 5, 2, 3))  #☻change bioclim numbers for the array
+  SpBioci  <- extract(raster(ListTifiles[j]), occ.unique)
+  j <- ifelse(j == 1, j, ifelse(j == 5, 2, 3))  # change bioclim numbers for the array
   thermal_indices[i, 1, j] <- mean(na.omit(SpBioci))
   thermal_indices[i, 2, j] <- sd(na.omit(SpBioci))
   thermal_indices[i, 3, j] <- quantile(SpBioci,0.95, na.rm = T)
@@ -45,7 +58,5 @@ for(j in c(1,5,6)){
 }
 save(thermal_indices, file = "F:/bat veolia mnhn/data/thermal indices/thermal_indices.bioclim")
 print(thermal_indices)
-cat(paste("---- ", Sys.time(), "Computing time for species", i, ": ", t0 - Sys.time(), "\n"))
+cat(paste("---- ", Sys.time(), "Computing time for species", i, ": ", Sys.time()-t0, "\n"))
 }
-
-
