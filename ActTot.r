c73216e88664bb96e0f1a80cc5e8723e3895e86e
YvesBas <yves.bas@gmail.com>
2019-07-02 11:49:21 +0200

    separate update and combine list

--- ActTot.r
+++ ActTot.r
@@ -3,18 +3,21 @@ SED="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/SelExportDonnees.r"
 EPD="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/extr_PF_DataLP.r"
 EPA="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/extr_PF_ActNuit.r"
 
-PP=c("55","56","57","58","59","5a","5b","5c")
+PP=c("55","56","57","58","59","5a","5b","5c","5d")
+PP2=c("55","56","57","58","59","5a","5b","5c","5d")
+
 #PP=c("5b")
 
 
 args="Valid"
-args[3]="Seuil90"
+args[3]="50"
 
 source(SED) #to save validated data
 
 for (i in 1:length(PP))
 {
-  args[1]="PrefPart"
+ print(paste(i,Sys.time()))
+   args[1]="PrefPart"
   args[2]=PP[i]
   Sys.time()
   source(SED) 
@@ -27,12 +30,10 @@ for (i in 1:length(PP))
   Sys.time()
 }
 
-PP=c("55","56","57","58","59","5a","5b")
-
 my.data=list()
 for (i in 1:length(PP))
 {
-  my.data[[i]]=fread(paste0("SpNuit2_",args[3],"_DataLP_PF_export_",PP[i],".csv"))
+  my.data[[i]]=fread(paste0("SpNuit2_",args[3],"_DataLP_PF_export_",PP2[i],".csv"))
 }
 ActTot=rbindlist(my.data)
 fwrite(ActTot,paste0("SpNuit2_",args[3],"_DataLP_PF_exportTot.csv"))
