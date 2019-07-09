005e2806c480245091864abface39f4a7c287a7c
YvesBas <yves.bas@gmail.com>
2019-07-09 16:36:09 +0200

    include a filter on channel

--- Act_SplitSp.R
+++ Act_SplitSp.R
@@ -3,13 +3,21 @@ library(data.table)
 DataAct=fread("./VigieChiro/pourTendances/data_vigieChiro_DataRP_SpTron_90_site_54sp_withAbs.csv")
 #DataActTron=fread("./VigieChiro/pourTendances/data_vigieChiro_DataRP_SpTron_90_TronPoint_54sp_withAbs.csv")
 #Sample=subset(DataActTron,DataActTron$participation=="57ac88dbdd4411000e4b764e")
-DirToCopy="./Vigiechiro/DataSp/RPCirw0_90"
+DirToCopy="./Vigiechiro/DataSp/RPCirw0_90_Direct"
+FiltDirect=T
 
+
+dir.create(DirToCopy)
 
 ListSp=unique(DataAct$espece)
 
 for (i in 1:length(ListSp))
 {
 DataSp=subset(DataAct,DataAct$espece==ListSp[i])
+if(FiltDirect)
+{
+  DataSp=subset(DataSp,DataSp$direct)
+}
 fwrite(DataSp,paste0(DirToCopy,"/",ListSp[i],".csv"))
 }
+
