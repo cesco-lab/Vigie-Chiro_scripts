a00ed389241b4afd13412a3af3abc04112bb523a
Romain Lorrilliere <romainlorrilliere@gmail.com>
2019-07-30 09:39:13 +0200

    add et repertory to record the model output

--- functions/GLMs/f_Sp_GLM_short.R
+++ functions/GLMs/f_Sp_GLM_short.R
@@ -37,13 +37,16 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
     ##Interactions=list(c(6,7,8),c(6,7,9),c(6,8,9),c(6,4))
     Interactions=interactions
     FormulaRandom=formulaRandom
+
+
     if(!is.na(asfactor)) {
         VarAbondance <- paste0(VarAbondance,ifelse(VarAbondance %in% asfactor,"_as_factor",""))
         VarSimple <- paste0(VarSimple,ifelse(VarSimple %in% asfactor,"_as_factor",""))
         if(!is.na(Interactions))
             for(l in 1:length(Interactions))
                  Interactions[[l]] <- paste0(Interactions[[l]],ifelse(Interactions[[l]] %in% asfactor,"_as_factor",""))
-        }
+    }
+
     ##FormulaRandom="+(1|espece)+(1|site)"
     ##FormulaRandom="+(1|espece)"
     SelSample=selSample #for testing computing time
@@ -66,14 +69,16 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
     if(is.null(repout)) repout <- "./VigieChiro/GLMs/"
     repoutSummary <- paste0(repout,"Summaries/")
     repoutLogs <- paste0(repout,"logs/")
-    repBackTransfomr <- paste0(repout,"forBackTransform/")
+    repoutBackTransform <- paste0(repout,"forBackTransform/")
+    repoutModel <- paste0(repout,"Model/")
     if(saveFig)repfig <- paste0(repout,"forBackTransform/")
 
     if(checkRepout) {
         dir.create(repout,showWarnings=FALSE)
         dir.create(repoutSummary,showWarnings=FALSE)
         dir.create(repoutLogs,showWarnings=FALSE)
-        dir.create(repBackTransfomr,showWarnings=FALSE)
+        dir.create(repoutBackTransform,showWarnings=FALSE)
+        dir.create(repoutModel,showWarnings=FALSE)
         if(saveFig) dir.create(repfig,showWarnings=FALSE)
     }
 
@@ -97,6 +102,7 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
 
     if(is.null(data)) SpNuit=fread(FAct) else SpNuit <- data
 
+
     if(!is.na(asfactor))
     {
         SpNuit=as.data.frame(SpNuit)
@@ -109,6 +115,10 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         SpNuit=as.data.table(SpNuit)
 
     }
+
+
+
+
     ##compute summaries of activity
     ColA=match(VarAbondance,names(SpNuit))
     Ab=as.data.frame(SpNuit)[,ColA]
@@ -117,13 +127,17 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
     AbwoNA=subset(Ab,!is.na(Ab))
 
 
+
     SpA1=aggregate(AbwoNA,by=list(SpNuitwoNA$espece),FUN=mean)
 
     if(length(unique(SpNuit$espece))>1) {
         barplot(SpA1$x,names.arg=SpA1$Group.1,las=2,cex.names=0.6)
     }
     SpPos=subset(SpNuitwoNA,AbwoNA>0)
-    AbPos=subset(Ab,Ab>0)
+
+
+       AbPos=subset(Ab,Ab>0)
+
     cat("\n Number of occurence:",(length(AbPos)),"\n")
     if(length(AbPos)<=length(VarSimple))
     {
@@ -196,7 +210,7 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         for (i in 1:length(VarSimple))
         {
             cat(i,"")
-            if(substr(VarSimple[i],1,5)=="poly(")
+             if(substr(VarSimple[i],1,5)=="poly(")
             {
                 Var=gsub("poly","",VarSimple[i])
                 Terms=tstrsplit(Var,split=",")
@@ -224,8 +238,13 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
                  timeAvancement <- format(timeAvancement, "%d-%m-%Y %HH%M")
                 cat("\n       ",timeAvancement,"  ... \n")}
 
+
+
+
         }
 
+
+    cat("DONE\n")
         end <- Sys.time() ## heure de fin
         diff <- end-start
         diff <- paste(round(diff,1),units(diff))
@@ -234,7 +253,7 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         forBackTransform=data.frame(cbind(VarList,Mean,Sdev))
 
 
-        csvfile <- paste0(repBackTransfomr,TagModel,".csv")
+        csvfile <- paste0(repoutBackTransform,TagModel,".csv")
         cat("  --> [CSV]:",csvfile)
         fwrite(forBackTransform,csvfile)
         cat("    DONE !\n")
@@ -260,7 +279,6 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         Formula=as.formula(paste0(FormulaFix_TtSp
                                  ,FormulaRandom))
 
-
         if(SelSample<nrow(SpNuit_Scale))
         {
             SpNuit_Sample=SpNuit_Scale[sample.int(nrow(SpNuit_Scale),SelSample),]
@@ -273,8 +291,12 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         cat("  ",format(start, "%d-%m-%Y %HH%M"),"  ...  ")
 
         if(printFormula) cat("\nglmmTMB(Formula= ",as.character(Formula)[2]," ",as.character(Formula)[1]," ",as.character(Formula)[3],", familiy= ",familyMod,")\n",sep="")
+
+
+
         ModSp=glmmTMB(Formula,data=SpNuit_Sample, family=familyMod)  #37 min
         if(doBeep) beep()
+
 
         end <- Sys.time() ## heure de demarage
         diff <- end-start
@@ -288,7 +310,7 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         Estimates=as.data.frame(coef(summary(ModSp))$cond)
         Estimates=cbind(term=row.names(Estimates),Estimates)
 
-        glmfile <- paste0(repout,TagModel,".glm")
+        glmfile <- paste0(repoutModel,TagModel,".glm")
         cat("  --> [GLM]:",glmfile)
         save(ModSp,file=glmfile)
         cat("    DONE !\n")
@@ -308,7 +330,7 @@ Sp_GLM_short=function(dataFile,varInterest,listEffects,interactions=NA
         fwrite(as.list(FormulaFix_TtSp),csvfile)
         cat("    DONE !\n")
 
-        csvfile <- paste0(repout,TagModel,"_",Suffix,"_Res.csv")
+        csvfile <- paste0(repoutModel,TagModel,"_",Suffix,"_Res.csv")
         cat("  --> [CSV]:",csvfile)
         fwrite(SpNuit_Sample,csvfile)
         cat("    DONE !\n")
