98eccdc1c8f76e9ec275f232e43a62cd79d7d449
statnmap <statnmap@users.noreply.github.com>
2019-11-09 11:32:24 +0100

    setup travis 3

--- tic.R
+++ tic.R
@@ -3,10 +3,8 @@ do_package_checks()
 if (ci_on_travis()) {
   get_stage("before_install") %>%
   add_code_step(
-    system(
-      "sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes & \\
-        sudo apt-get --yes --force-yes update -qq & \\
-        sudo apt-get install --yes libudunits2-dev libproj-dev libgeos-dev libgdal-dev"))
+    # Ubuntugis already in travis
+    system("sudo apt-get install --yes libudunits2-dev libproj-dev libgeos-dev libgdal-dev"))
 
   do_pkgdown()
 }
