e351c7828318b1414b7a41ff0ecceaf50c18629a
YvesBas <yves.bas@gmail.com>
2020-06-22 15:54:23 +0200

    debug update export table

--- misc/ActNew.r
+++ misc/ActNew.r
@@ -1,35 +1,37 @@
 library(data.table)
-SED="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/SelExportDonnees.r"
-EPD="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/extr_PF_DataLP.r"
-EPA="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/extr_PF_ActNuit.r"
+SED="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/misc/SelExportDonnees.r"
+EPD="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/misc/extr_PF_DataLP.r"
+EPA="./Tadarida/Vigie-Chiro_scripts/Vigie-Chiro_scripts/misc/extr_PF_ActNuit.r"
 
-PP=c("55","56","57","58","59","5a","5b","5c","5d")
+PP=c("55","56","57","58","59","5a","5b","5c","5d","5e","5f")
 
 #PP=c("5b")
 
 
 args="Valid"
 Thresholds=c(90,50)
-args[10]="G:/VigieChiro/Raw/New"
+args[10]="E:/VigieChiro/Raw/New"
 args[12]=""
 args[13]=""
 args[14]=""
-FRaw="G:/VigieChiro/Raw"
-FDT="C:/wamp64/www/export_190907_190924.txt"
-
+FRaw="E:/VigieChiro/Raw"
+FDT="C:/wamp64/www/export_200512_200616.txt"
+#FDT="E:/exporttot200409.txt"
 
 args[3]=Thresholds[1]
 
-source(SED) #to save validated data
+#source(SED) #to save validated data
 
-for (i in 1:length(PP))
+dir.create(args[10])
+
+for (z in 1:length(PP))
 {
-  print(paste(i,Sys.time()))
+  print(paste(z,Sys.time()))
   args[1]="PrefPart"
   
   for (j in c(c(0:9),"a","b","c","d","e","f"))
   {
-    args[2]=paste0(PP[i],j)
+    args[2]=paste0(PP[z],j)
     print(args[2])
     Sys.time()
     source(SED) 
@@ -62,9 +64,21 @@ for (i in 1:length(Fnew1))
   if(nrow(TabNew)>0)
   {
     PartNew=levels(as.factor(TabNew$participation))
-    TabOld=fread(paste0(dirname(args[10]),"/",basename(Fnew1[i])))
-    TabPurge=subset(TabOld,!(TabOld$participation %in% PartNew))
-    TabUpdate=rbind(TabPurge,TabNew)
+    #PartNew=unique(TabNew$participation)
+      FileOld=paste0(dirname(args[10]),"/",basename(Fnew1[i]))
+    if(file.exists(FileOld))
+    {
+      TabOld=fread(paste0(dirname(args[10]),"/",basename(Fnew1[i])))
+      #ExistingParticipation=unique(TabOld$participation)
+      #ParticipationToKeep=subset(ExistingParticipation
+       #                          ,(ExistingParticipation %in% PartNew))
+      
+      TabPurge=subset(TabOld,!(TabOld$participation %in% PartNew))
+      TabUpdate=rbind(TabPurge,TabNew)
+    }else{
+      TabUpdate=TabNew
+    }
+    
     fwrite(TabUpdate,paste0(dirname(args[10]),"/",basename(Fnew1[i])))
     print(basename(Fnew1[i]))
     print(nrow(TabNew))
@@ -78,6 +92,7 @@ Fnew1=subset(Fnew1,substr(basename(Fnew1),1,1)=="D")
 for (i in 1:length(Fnew1))
 {
   TabNew=fread(Fnew1[i])
+  print(ncol(TabNew))
   if(nrow(TabNew)>0)
   {
     PartNew=levels(as.factor(TabNew$participation))
@@ -86,7 +101,12 @@ for (i in 1:length(Fnew1))
     {
       TabOld=fread(OldFile)
       TabPurge=subset(TabOld,!(TabOld$participation %in% PartNew))
+      if(ncol(TabNew)>ncol(TabOld))
+      {
+        TabNew=subset(TabNew,select=colnames(TabOld))
+      }  
       TabUpdate=rbind(TabPurge,TabNew)
+      
     }else{
       TabUpdate=TabNew
     }
