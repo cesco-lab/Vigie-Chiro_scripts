7d54178af0a9a87d37cf0ee6d5df168923012c90
YvesBas <yves.bas@gmail.com>
2023-03-27 14:43:40 +0200

    include directory suppression

---
+++ misc/Imports/wav_unblock2.R
@@ -0,0 +1,27 @@
+Dir="D:/PVAL_Espousouilles"
+
+AllFiles=list.files(Dir,full.names=T,recursive=T)
+
+Blocs=subset(AllFiles,grepl("bloc",basename(dirname(AllFiles))))
+
+UnBlock=paste0(dirname(dirname(Blocs)),"/",basename(Blocs))
+
+file.rename(from=Blocs,to=UnBlock)
+
+AllDirs=list.dirs(Dir)
+
+BlocDir=subset(AllDirs,grepl("bloc",basename(AllDirs)))
+
+PartBlocDir=unique(dirname(BlocDir))
+
+for (i in 1:length(BlocDir)){
+  bfi=list.files(BlocDir[i])
+  if(length(bfi)==0){
+    test=unlink(BlocDir[i],force=T,recursive=T)
+    
+  }else{
+    stop("pb dossier non vide")
+  }
+  
+  
+}
